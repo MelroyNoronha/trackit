5bd424f8e2b364bd0833d694f59661e628f5e7d4
MelroyNoronha <melmax1208@gmail.com>
2021-05-05 18:44:54 +0530

    created CustomTextInput component

---
+++ src/components/common/CustomTextInput.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import {TextInput, StyleSheet} from 'react-native';
+
+import colors from '../../utils/colors';
+
+const CustomTextInput = props => {
+  const {value, onChangeText, placeholder, keyboardType} = props;
+
+  const [borderColor, setBorderColor] = useState(colors.borderGrey);
+
+  const handleInputFocus = () => {
+    setBorderColor(colors.primary);
+  };
+
+  const handleInputBlur = () => {
+    setBorderColor(colors.borderGrey);
+  };
+
+  return (
+    <TextInput
+      style={[
+        styles.textInput,
+        {
+          borderColor,
+        },
+      ]}
+      onChangeText={onChangeText}
+      value={value}
+      placeholder={placeholder}
+      placeholderTextColor={colors.borderGrey}
+      keyboardType={keyboardType}
+      onFocus={handleInputFocus}
+      onBlur={handleInputBlur}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  textInput: {
+    borderWidth: 1,
+    borderRadius: 10,
+    width: '90%',
+    marginVertical: 15,
+    paddingHorizontal: 15,
+    color: colors.darkText,
+  },
+});
+
+export default CustomTextInput;
