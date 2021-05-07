cdd922778b9714d20b3b6ec9f2c5cfe75e70f8a1
MelroyNoronha <melmax1208@gmail.com>
2021-05-07 19:34:56 +0530

    create initTrackItApp function
    remove dummyData
    get data from AsyncStorage
    if data does not exist in AsyncStorage, set it to empty array

--- src/App.js
+++ src/App.js
@@ -1,43 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Home from './components/Home';
 
-const dummyData = [
-  {
-    title: 'Today',
-    data: [
-      {
-        isExpense: true,
-        isIncome: false,
-        description: 'Car tyre change',
-        value: '$329',
-        date: new Date(),
-      },
-      {
-        isExpense: true,
-        isIncome: false,
-        description: 'Furniture',
-        value: '$4,000',
-        date: new Date(),
-      },
-    ],
-  },
-  {
-    title: 'April 12, 2021',
-    data: [
-      {
-        isExpense: false,
-        isIncome: true,
-        description: 'Salary',
-        value: '$7,329',
-        date: new Date('2021-04-12'),
-      },
-    ],
-  },
-];
-
 const App = () => {
-  return <Home data={dummyData} />;
+  const [trackItData, setTrackItData] = useState([]);
+
+  useEffect(() => {
+    const initTrackItApp = async () => {
+      try {
+        const trackItData = await AsyncStorage.getItem('@trackItData');
+
+        if (trackItData !== null) {
+          const trackItDataParsed = JSON.parse(trackItData);
+          setTrackItData(trackItDataParsed);
+        } else {
+          await AsyncStorage.setItem('@trackItData', JSON.stringify([]));
+          setTrackItData([]);
+        }
+      } catch (e) {
+        alert('Something went wrong. Please restart the application.');
+      }
+    };
+
+    initTrackItApp();
+  }, []);
+
+  return <Home data={trackItData} />;
 };
 
 export default App;
